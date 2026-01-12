b8b9aee1093c5475b52f3d580971f5d877dcfd36
bhaskar <bhaskarc2468@gmail.com>
2026-01-12 21:55:57 +0530

    responsisve error changes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,34 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import "./index.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <header className="navbar">
       <div className="nav-container">
         <div className="nav-logo">Portfolio</div>
 
-        <nav>
+        {/* Hamburger */}
+        <div
+          className={`hamburger ${menuOpen ? "open" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <nav className={menuOpen ? "nav-open" : ""}>
           <ul className="nav-links">
-            <li><NavLink to="/" end>Home</NavLink></li>
-            <li><NavLink to="/services">Service</NavLink></li>
-            <li><NavLink to="/resume">Resume</NavLink></li>
-            <li><NavLink to="/projects">Projects</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><NavLink to="/" end onClick={closeMenu}>Home</NavLink></li>
+            <li><NavLink to="/services" onClick={closeMenu}>Service</NavLink></li>
+            <li><NavLink to="/resume" onClick={closeMenu}>Resume</NavLink></li>
+            <li><NavLink to="/projects" onClick={closeMenu}>Projects</NavLink></li>
+            <li><NavLink to="/contact" onClick={closeMenu}>Contact</NavLink></li>
           </ul>
         </nav>
       </div>
